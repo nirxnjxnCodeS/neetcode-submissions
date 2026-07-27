014243114516c2b773c0c8a79dd80b013127c5e1
Niranjan S Binu <142873511+nirxnjxnCodeS@users.noreply.github.com>
2026-07-27 12:16:11 +0530

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (int i = 0; i < strs.length; i++) {
+            String word = strs[i];
+
+            char[] chars = word.toCharArray();
+            Arrays.sort(chars); 
+
+            String sortedWord = new String(chars);
+
+            if (!map.containsKey(sortedWord)) {
+                map.put(sortedWord, new ArrayList<>());
+            }
+
+            map.get(sortedWord).add(word);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
