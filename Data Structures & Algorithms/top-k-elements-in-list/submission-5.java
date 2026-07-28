be1b722e7b0dc9481d009e9d1c0b234b2f72e319
Niranjan S Binu <142873511+nirxnjxnCodeS@users.noreply.github.com>
2026-07-28 12:05:29 +0530

    Add: top-k-elements-in-list - submission-5

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-5.java
@@ -0,0 +1,66 @@
+public class Solution {
+
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> count = new HashMap<Integer, Integer>();
+
+        for(int i=0;i<nums.length;i++){
+            int num = nums[i];
+
+            if(count.containsKey(num)){
+                int frequency=count.get(num);
+                count.put(num, frequency + 1);
+            }
+            else{
+                count.put(num, 1);
+            }
+        }
+
+        //Map to List
+        List<Integer>[] buckets = new ArrayList[nums.length + 1];
+
+        List<Map.Entry<Integer, Integer>> entries = new ArrayList<Map.Entry<Integer,Integer>>(count.entrySet());
+
+        for(int i=0;i<entries.size();i++){
+            Map.Entry<Integer, Integer> entry = entries.get(i);
+
+            int number = entry.getKey();
+            int frequency = entry.getValue();
+
+            if(buckets[frequency]==null){
+                buckets[frequency]= new ArrayList<Integer>();
+            }
+            buckets[frequency].add(number);
+
+        }
+
+        int[] result = new int[k];
+
+        int index=0;
+
+        for (int frequency = buckets.length - 1; frequency >= 0; frequency--) {
+
+            if (buckets[frequency] != null) {
+
+                for (int j = 0; j < buckets[frequency].size(); j++) {
+
+                    result[index] = buckets[frequency].get(j);
+
+                    index++;
+
+                    if (index == k) {
+                        return result;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+}
+
+
+
+
+
+
+
